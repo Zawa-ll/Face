9f05e5c00d41ff2758fe2569695e42c6e67fdf80
Zawa_ll <zawalee33@gmail.com>
2024-03-29 09:17:05 -0400

    Function for recording video added

--- main.cpp
+++ main.cpp
@@ -73,14 +73,18 @@ int camera()
         if (is_face)
         {
             // TODO:
-            // vector<cv::Rect> faces; // pixles for frontal face, vector for multiple faces
-            // face_model.detectMultiScale(frame,
-            //                             faces, 1.1, 2, 0 | cv::CASCADE_SCALE_IMAGE,
-            //                             cv::Size(30, 30));
+            vector<cv::Rect> faces; // pixles for frontal face, vector for multiple faces
+            face_model.detectMultiScale(frame,
+                                        faces, 1.1, 2, 0 | cv::CASCADE_SCALE_IMAGE,
+                                        cv::Size(30, 30));
 
-            // for (int i = 0; i < faces.size(); i ++ ) {
+            for (int i = 0; i < faces.size(); i++)
+            {
+                cv::Point center(faces[i].x + faces[i].width / 2, faces[i].y + faces[i].height / 2); // Get the center of faces[i]
+                ellipse(fame, center, cv::Size(faces[i].width / 2, faces[i].height / 2), 0, 0, 360, cv : Scalar(255, 0, 255), 4, 8, 0);
+            }
 
-            // }
+            cv::imshow("video", frmae);
         }
 
         cv::imshow("video", frame);
@@ -89,6 +93,37 @@ int camera()
 
         if (c == 27) // ESC on Keyboard: ascii = 27
             break;
+    }
+
+    return 0;
+}
+
+int rec_camera()
+{
+    int camera_id = 0;
+    cv::VideoCapture cap;
+    cap.open(camera_id);
+    cv::Mat frame;
+
+    cv::VideoWriter output_video;
+
+    int width = cap.get(cv::CAP_PROP_FRAME_WIDTH);
+    int height = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
+    output_video.open("%d:/rec.avi", CV_FOURCC("M", "J", "P", "G", 30.0, cv::Size(width, height)));
+
+    for (;;)
+    {
+        cap >> frame;
+        if (frame.empty())
+            break;
+
+        cv::imshow("video", frame); // Output one bit of video each time
+
+        output_video.write(frame);
+
+        char c = (char)cv::waitKey(10) // Waiting for Key from keyboard every 10 ms
+
+            if (c == 27) break;
     }
 
     return 0;
