86691a771e9843339b96fd0209fb44a5418b9a74
Zawa_ll <zawalee33@gmail.com>
2024-03-29 09:51:10 -0400

    Function for fetch a single pixel from a img

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
 // Method for showing an image
 int show_img()
 {
-    cv::Mat img = cv::imread("tools/cat.png", -1);
+    cv::Mat img = cv::imread("data/cat.png", -1);
 
     if (img.empty())
         return -1;
@@ -73,7 +73,7 @@ int camera()
         if (is_face)
         {
             // TODO:
-            vector<cv::Rect> faces; // pixles for frontal face, vector for multiple faces
+            vector<cv::Rect> faces; // pixels for frontal face, vector for multiple faces
             face_model.detectMultiScale(frame,
                                         faces, 1.1, 2, 0 | cv::CASCADE_SCALE_IMAGE,
                                         cv::Size(30, 30));
@@ -125,6 +125,105 @@ int rec_camera()
 
             if (c == 27) break;
     }
+
+    return 0;
+}
+
+int slider()
+{
+    cv::namedWindow("slider", cv::WINDOW_AUTOSIZE);
+
+    g_cap.open(string("data/test.avi");)
+
+        int frames = (int)g_cap.get(VC_CAP_PROP_FRAME_COUNT);
+    int tmpw = (int)g_cap.get(CV_CAP_PROP_FRMAE_WIDTH);
+    int tmph = (int)g_cap.get(CV_CAP_PROP_FRAME_HEIGHT);
+
+    cout << "Video has " << frames << " frmaes of dimensions of (" << tmpw << ", " << tmph << ")." << endl;
+
+    cv::createTrackbar(
+        "Position",
+        "slider",
+        &g_slider_position,
+        frames,
+        onTrackbarSlide);
+
+    cv::Mat frame;
+
+    for (;;)
+    {
+        if (g_run != 0)
+        {
+            g_cap >> frame;
+            if (frame.empty())
+                break;
+            int current_pos = (int)g_cap.get(CV_CAP_PROP_POS_FRAMES);
+            g_dontset = 1;
+
+            cv::setTrackbarPos("Position", "slider", current_pos);
+            cv::imshow("slider", frame);
+            g_run -= 1;
+        }
+
+        char c = (char)cv::waitKey(10);
+
+        if (c == 's')
+        { // single step mode
+            g_run = 1;
+            cout << "Single step, run = " << g_run << endl;
+        }
+
+        if (c == 'r')
+        {
+            g_run = -1;
+            cout << "Run mode, run = " << g_run << endl;
+        }
+
+        if (c == 27)
+            break;
+    }
+}
+
+int pixels()
+{
+    cv::Mat img_rb, img_gry, img_cny, img_pyr, img_pyr2;
+
+    cv::namedWindow("Gray", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("Canny", cv::WINDOW_AUTOSIZE);
+
+    img_rgb = cv::imread("data/cat.png");
+
+    // Convert color img to gray img
+    cv::cvtColor(img_rgb, img_gry, cv::COLOR_BGR2GRAY);
+
+    cv::pyrDown(img_gry, img_pyr); // downsize the img
+    cv::pyrDown(img_pyr, img_pyr2);
+
+    // Fetch the boarder of a img
+    cv::Canny(img_pyr2, img_cny, 10, 100, 3, true);
+
+    int x = 16, y = 32;
+    cv::Vec3b intensity = img_rgb.at<cv::Vec3b>(y, x); // Fetch pixel in mode of Vec3b (3 val in color pixel)
+
+    uchar blue = intensity[0];
+    uchar green = intensity[1];
+    uchar red = intensity[2];
+
+    cout << "At (x, y) = (" << x << "," << y << "): (b,g,r) = (" << (unsigned int)blue << "," << (unsigned int)green << "," << (unsigned int)red << ")" << end;
+
+    cout << "Gray pixel there is: " << (unsigned int)img_gry.at<uchar>(y, x) << endl; // Fetch pixel in mode of uchar (only 1 val in grey pixel)
+
+    x /= 4;
+    y /= 4;
+
+    cout << "Pyramid2 pixel there is" << (unsigned int)img_pyr2.at<uchar>(y, x) << endl;
+
+    img_cny.at<uchat>(x, y) = 128; // Set the Canny pixel in the value of 128
+
+    cv::imshow("Gray", img_gry);
+    cv::imshow("Canny", img_cny);
+
+    cv::waitKey(0);
 
     return 0;
 }
